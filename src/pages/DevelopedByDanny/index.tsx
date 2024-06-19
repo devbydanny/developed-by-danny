fb7a4282fd19c2b755efd1f419af636a22d3d0c3
dannyheney <dheney99@gmail.com>
2024-06-19 11:49:39 +0100

    fixed homepage height

--- src/pages/DevelopedByDanny/index.tsx
+++ src/pages/DevelopedByDanny/index.tsx
@@ -8,30 +8,33 @@ const DevelopedByDanny: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <FlexBox className="background" height="100vh" width="100%">
-        <FlexBox className="grain" height="100vh" width="100%" />
-      </FlexBox>
-      <FlexBox
-        height="100vh"
-        width="100%"
-        justifyContent="center"
-        alignItems="center"
-        color="white"
-        style={{ position: 'relative', top: '-100vh' }}
-      >
-        <Text
-          textType="p"
-          fontFamily="KryptonMedium"
-          fontWeight={500}
-          fontSize="calc(2vw + 2vh)"
-          onClick={() => navigate('/home')}
-          style={{ cursor: 'pointer' }}
+    <FlexBox
+      className="background"
+      flexDirection="column"
+      height="100vh"
+      width="100%"
+    >
+      <FlexBox className="grain" height="100vh" width="100%">
+        <FlexBox
+          height="100vh"
+          width="100%"
+          justifyContent="center"
+          alignItems="center"
+          color="white"
         >
-          DEVELOPED BY DANNY
-        </Text>
+          <Text
+            textType="p"
+            fontFamily="KryptonMedium"
+            fontWeight={500}
+            fontSize="calc(2vw + 2vh)"
+            onClick={() => navigate('/home')}
+            style={{ cursor: 'pointer' }}
+          >
+            DEVELOPED BY DANNY
+          </Text>
+        </FlexBox>
       </FlexBox>
-    </>
+    </FlexBox>
   );
 };
 
