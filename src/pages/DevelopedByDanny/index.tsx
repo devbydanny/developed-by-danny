77a5db3290d249b87d0c9903af42bcb88b2ce99f
dannyheney <dheney99@gmail.com>
2024-06-19 11:32:44 +0100

    add animated css bg

--- src/pages/DevelopedByDanny/index.tsx
+++ src/pages/DevelopedByDanny/index.tsx
@@ -2,28 +2,36 @@ import React from 'react';
 import Text from '../../components/Text';
 import { useNavigate } from 'react-router-dom';
 import FlexBox from '../../components/FlexBox';
+import './DevelopedByDanny.css';
 
 const DevelopedByDanny: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <FlexBox
-      height="100vh"
-      width="100%"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Text
-        textType="p"
-        fontFamily="KryptonMedium"
-        fontWeight={500}
-        fontSize="calc(4vw + 4vh)"
-        onClick={() => navigate('/home')}
-        style={{ cursor: 'pointer' }}
+    <>
+      <FlexBox className="background" height="100vh" width="100%">
+        <FlexBox className="grain" height="100vh" width="100%" />
+      </FlexBox>
+      <FlexBox
+        height="100vh"
+        width="100%"
+        justifyContent="center"
+        alignItems="center"
+        color="white"
+        style={{ position: 'relative', top: '-100vh' }}
       >
-        DEVELOPED BY DANNY
-      </Text>
-    </FlexBox>
+        <Text
+          textType="p"
+          fontFamily="KryptonMedium"
+          fontWeight={500}
+          fontSize="calc(2vw + 2vh)"
+          onClick={() => navigate('/home')}
+          style={{ cursor: 'pointer' }}
+        >
+          DEVELOPED BY DANNY
+        </Text>
+      </FlexBox>
+    </>
   );
 };
 
